1b3e42ec4b780972e92bbfa4852e92d380e6e3b4
kungjiajun <34576346+kungjiajun@users.noreply.github.com>
2022-08-16 21:57:38 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,43 +1,34 @@
-/* ************************************************************************
-> File Name:     main.cpp
-> Author:        程序员Carl
-> 微信公众号:    代码随想录
-> Created Time:  Sun Dec  2 20:21:41 2018
-> Description:   
- ************************************************************************/
 #include <iostream>
-#include "skiplist.h"
-#define FILE_PATH "./store/dumpFile"
+#include "SkipList.h"
 
 int main() {
+    SkipList<int, std::string> skiplist(32);
+    skiplist.insert_element(1, "一");
+    skiplist.insert_element(2, "二");
+    skiplist.insert_element(3, "三");
+    skiplist.insert_element(4, "四");
+    skiplist.insert_element(5, "五");
+    skiplist.insert_element(10, "十");
+    skiplist.insert_element(8, "八");
+    skiplist.insert_element(20, "二十");
 
-    // 键值中的key用int型，如果用其他类型，需要自定义比较函数
-    // 而且如果修改key的类型，同时需要修改skipList.load_file函数
-    SkipList<int, std::string> skipList(6);
-	skipList.insert_element(1, "学"); 
-	skipList.insert_element(3, "算法"); 
-	skipList.insert_element(7, "认准"); 
-	skipList.insert_element(8, "微信公众号：代码随想录"); 
-	skipList.insert_element(9, "学习"); 
-	skipList.insert_element(19, "算法不迷路"); 
-	skipList.insert_element(19, "赶快关注吧你会发现详见很晚！"); 
+    std::cout << "skipList size:" << skiplist.size() << std::endl;
 
-    std::cout << "skipList size:" << skipList.size() << std::endl;
+    skiplist.query_element(10);
+    skiplist.query_element(1);
+    skiplist.query_element(18);
 
-    skipList.dump_file();
+    skiplist.dump_file();
 
-    // skipList.load_file();
+    skiplist.display_list();
 
-    skipList.search_element(9);
-    skipList.search_element(18);
+    skiplist.delete_element(3);
+    skiplist.delete_element(7);
+    skiplist.delete_element(2);
 
+    std::cout << "skipList size:" << skiplist.size() << std::endl;
 
-    skipList.display_list();
+    skiplist.display_list();
 
-    skipList.delete_element(3);
-    skipList.delete_element(7);
-
-    std::cout << "skipList size:" << skipList.size() << std::endl;
-
-    skipList.display_list();
+    return 0;
 }
